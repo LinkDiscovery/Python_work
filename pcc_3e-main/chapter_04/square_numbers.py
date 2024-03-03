43d38158a63bf56c063390eafd7b228438daa201
LinkDiscovery <shin920111@gmail.com>
2024-03-03 18:13:44 +0900

    0303

--- pcc_3e-main/chapter_04/square_numbers.py
+++ pcc_3e-main/chapter_04/square_numbers.py
@@ -1,5 +1,28 @@
-squares = []
-for value in range(1, 11):
-    squares.append(value**2)
+# squares = []
+# for value in range(1, 11):
+#     squares.append(value**2)
 
-print(squares)
+# print(squares)
+
+numbers = list(range(1,100))
+maximum_value = max(numbers)
+print(maximum_value)
+print(numbers[-5:-2])
+
+a = [1,2,3],[4,5]
+b = ['a','b','c'],['d','e']
+c=a[:]
+d=b[:]
+a[0][0] = 6
+b[0][0] = 'f'
+print(a)
+print(b)
+
+print(c)
+print(d)
+
+
+
+
+    
+    
