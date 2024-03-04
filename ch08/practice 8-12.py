7d166b5a326e79799689aced32376267a44900ad
LinkDiscovery <shin920111@gmail.com>
2024-03-04 10:16:50 +0900

    0304

--- ch08/practice 8-12.py
+++
@@ -1,9 +0,0 @@
-def make_sandwich(*ingredients):
-    """요청 재료 받기"""
-    print("\n Ingredient Order List")
-    for ingredient in ingredients:
-        print(f"-{ingredient}")
-    
-make_sandwich('ham')
-make_sandwich('ham','cheese','eggs')
-make_sandwich('eggs','bacon')
