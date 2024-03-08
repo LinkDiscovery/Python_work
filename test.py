291de5c47144697512b69197f92d6fa65e1d014a
LinkDiscovery <shin920111@gmail.com>
2024-03-08 10:01:06 +0900

    0308

--- test.py
+++ test.py
@@ -1,11 +1,41 @@
-# a = input()
-# a = int(a)
+# a= {1:"A+",
+#     2:"b+",
+#     3:"c+"}
 
-# for i in range(1,10):
-#     print(f"{a} * {i} = {a*i}")
-    
-a = input()
-a = int(a)
+# for a1 in a : 
+#     print(a[a1])
 
-for i in range(1,a+1):
-	print(f"*"*i)
+# b = {
+# 	3:'b+',
+#     6:'a+',
+#     9:'s+'
+# }
+# print(b)
+# b[3]
+
+# first = lambda x : x[0]
+# second = lambda x : x[1]
+
+# for row in b.items():
+# 	print(first(row), second(row))
+ 
+# sorted(b.items(), key=lambda x:x[1])
+
+a=0
+b=30
+
+c = (a*60)+b-45
+hr = int(c/60)
+mi = int(c%60)
+
+if (a==0):
+    a=24
+    c = (a*60)+b-45
+    hr = int(c/60)
+    mi = int(c%60)
+
+    print(f"{hr} {mi}") 
+
+else:
+    print(f"{hr} {mi}") 
+
